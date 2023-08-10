9d94c34a61fcce651f1a755a6ed968713a8cbd6e
isaiahcash <isaiahcash@gmail.com>
2023-08-09 21:52:41 -0400

    First commit

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Version 1.0.0</h3>
+  <router-link to="/">Go to Home</router-link>
+</template>
+
+<script>
+export default {
+  name: "About",
+  props: {
+    showAddTask: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
